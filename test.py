62c4434c9a50990803fb755e874aa1fd4c660ee2
YourUserName <Your@Email.address>
2021-09-27 22:21:05 +0400

    test fail

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
-import unittest
+import turtle
 
-class draw_square(unittest.TestCase):
+import assignmet
 
-
-    def test_draw_square(self):
-        self.assertEqual('90,90,90,90'.upper(), '90,90,90,90')
-
-def test_draw_square(self):
-        self.assertTrue('90,90,90,90'.isupper())
-        self.assertFalse('90,90,90,90'.isupper())
+def test_rectangle_will_fit()
+    assigment.rectangle_will_fit(10,20)
+    assert(turtle.xcor()==10)
+    assert(turtle.ycor()==20)
+    assert(turtle.length()==400)
+    assert(turtle.height()==400)
