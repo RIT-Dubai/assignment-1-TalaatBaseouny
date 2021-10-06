6f8e87830735ff4f29788c9f6388628d11d6a42a
YourUserName <Your@Email.address>
2021-10-07 00:02:50 +0400

    part 4 update

--- project.py
+++ project.py
@@ -31,3 +31,30 @@ for i in range(3):
     turtle.forward(225)
     turtle.left(120)
 turtle.end_fill()
+
+
+def draw_circle(c ,l, w):
+    turtle.penup()
+turtle.goto(10,-200)
+turtle.pendown()
+turtle.color("green")
+turtle.begin_fill()
+turtle.circle(80)
+turtle.end_fill()
+
+
+def draw_rectangle (s,h,a,a2,c):
+   turtle.penup()
+turtle.goto(200,-100)
+turtle.pendown()
+turtle.color("red")
+turtle.begin_fill()
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(80)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(80)
+turtle.left(90)
+turtle.end_fill()
