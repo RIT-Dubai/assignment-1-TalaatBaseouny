48c90a51ef2bd0714244c6791f7abbabc36f39d1
YourUserName <Your@Email.address>
2021-10-06 19:33:04 +0400

    step 1

---
+++ project.py
@@ -0,0 +1,27 @@
+import turtle
+
+turtle.speed(0)
+
+
+#def border();
+turtle.penup()
+turtle.goto(-400,-400)
+turtle.pendown()
+turtle.goto(400,-400)
+turtle.goto(400,400)
+turtle.goto(-400,400)
+turtle.goto(-400,-400)
+turtle.penup()
+turtle.goto(0,0)
+turtle.pendown()
+turtle.begin_fill()
+turtle.fillcolor()
+
+
+
+
+
+
+turtle.hideturtle()
+turtle.exitonclick()
+
