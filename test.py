231c23d248d90fa28eaf947a6c927cef9e7f36fd
YourUserName <Your@Email.address>
2021-10-07 11:02:45 +0400

    test 1

--- test.py
+++ test.py
@@ -2,19 +2,11 @@ import turtle
 
 import project
 
-def test_rectangle_will_fit():
-    project.test_rectangle_will_fit(200,-100)
-    assert(turtle.xcor()==200)
-    assert(turtle.ycor()==-100)
-
-
-def test_triangle_will_fit():
-    project.test_triangle_will_fil(-360,200)
+def test_draw_triangle():
+    project.test_draw_triangle(-360,200)
     assert(turtle.xcor()==-360)
     assert(turtle.ycor()==200)
 
 
-def test_cricle_will_fit():
-    project.test_cricle_will_fit(10,-200)
-    assert(turtle.xcor()==10)
-    assert(turtle.ycor()==-200)
+
+
